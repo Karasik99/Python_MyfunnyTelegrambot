b0b98b8d771780d22492dfbdd109eb7c508d669b
NIkita <nikita.carasevitch@yandex.by>
2022-06-26 15:35:41 +0300

    DRF

---
+++ DRF/DRF/onlineshop/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Product
+        fields = '__all__'
